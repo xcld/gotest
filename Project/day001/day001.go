e3d2be227061ac50bc4463c3a56ac69839bd805f
xcld <hfylwj@163.com>
2022-11-23 00:27:30 +0800

    202211230027

--- Project/day001/day001.go
+++ Project/day001/day001.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+)
+
+var _ int = 18
+var Gender, Height, Weight int = 0, 178, 48
+var (
+	Bust  float32 = 90
+	Waist float32 = 60
+	Hip   float32 = 90
 )
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6}
-	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
-	}
-	for j := 0; j < 5; j++ {
-		for i := 0; i < 10; i++ {
-			if i > 5 {
-				break
-			}
-			fmt.Println(i)
-		}
-	}
+	Scale := float32(5 / 8)
+	fmt.Printf("国际时装周模特%d标准为:\n%dcm的身高\n，%dkg的体重\n，三围：\n胸围为：%fcm\n,腰围为：%fcm\n,臀围为：%fcm\n，并且上下身比例为：%f\n", Gender, Height, Weight, Bust, Waist, Hip, Scale)
+	//fmt.Printf("It's over %f\n", Power)
+	fmt.Println(reflect.TypeOf(Scale))
 }
