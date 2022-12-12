8957334945708ad1c68d8b18c53df05935150c76
疏影横斜一片疏影 <1349766697@qq.com>
2022-12-12 17:59:51 +0800

    2022.12.12练习题

--- Project/day001/homework2.go
+++ Project/day001/homework2.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	//	练习题
 	//	1.判断 100300 - 100500之间有多少个素数，并输出所有素数。
-	for i := 100300; i <= 100500; i++ {
-		flag_ := 1
-		for j := 2; j < i; j++ {
-			if i%j == 0 {
-				flag_ = 0
-			}
-		}
-		if flag_ == 1 {
-			fmt.Println("素数为： ", i)
-		}
-	}
+	//for i := 100300; i <= 100500; i++ {
+	//	flag_ := 1
+	//	for j := 2; j < i; j++ {
+	//		if i%j == 0 {
+	//			flag_ = 0
+	//		}
+	//	}
+	//	if flag_ == 1 {
+	//		fmt.Println("素数为： ", i)
+	//	}
+	//}
 	fmt.Println()
 	fmt.Println()
 	//	2.打印出100-1000000000 中所有的”自幂数”。所谓“自幂数”在一个固定的进制中，一个n位自然数等于自身各个数位上数字的n次幂之和
@@ -29,13 +29,61 @@ func main() {
 	//七位的北斗七星数共有4个: 1741725，4210818，9888817，9926315;
 	//八位的八仙数共有3个: 24678850，24678851，88593477;
 	//九位的九九重阳数共有4个: 146511208，472335975，534494836，912985153;
-	for i := 100; i < 10000000; i++ {
-		flag_ := 0
-		flag_ = Is_zimishu(i)
-		if flag_ == 1 {
-			fmt.Println("自幂数为：  ", i)
-		}
+
+	//for i := 100; i < 10000000; i++ {
+	//	flag_ := 0
+	//	flag_ = Is_zimishu(i)
+	//	if flag_ == 1 {
+	//		fmt.Println("自幂数为：  ", i)
+	//	}
+	//}
+
+	//3. 写一个计算n的阶乘之和程序并输出
+	//var v int
+	//fmt.Println("请输入一个整数: ")
+	//fmt.Scanf("%d", &v)
+	//sum := 1
+	//for i := 1; i <= v; i++ {
+	//	sum *= i
+	//}
+	//fmt.Println("阶乘的和为： ", sum)
+
+	//4. 任意输入三条边，并判断能否组成三角形，若能，判断三角形的类型和计算三角形的面积。
+	//两条较短边的平方和大于最长边的平方，此三角形就是锐角三角形；
+	//两条较短边的平方和小于最长边的平方，此三角形就是钝角三角形；
+	//两条边短边的平方和等于最长边的平方，此三角形就是直角三角形.
+	var a, b, c int
+	fmt.Println("请输入三条边: ")
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+	c = a
+	fmt.Println(a, b, c)
+	max, mid, min := a, a, a
+	if b >= max {
+		max = b
+	} else if max >= b && b >= min {
+		mid = b
+		b = mid
+	} else {
+		min = b
 	}
+
+	if c >= max {
+		max = c
+	} else if max >= c && c >= min {
+		mid = c
+	} else {
+		min = c
+	}
+
+	fmt.Println(min, mid, max)
+
+	if (a+b) > c && (a+c) > b && (c+b) > a {
+		fmt.Println("是三角形")
+
+	} else {
+		fmt.Println("不能构成三角形")
+	}
+
 }
 
 // 判断是否是自幂数
